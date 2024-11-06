5723e7e43cdbbc2865a55e8767c3d4c085a0a050
Michael L Heuer <heuermh@acm.org>
2024-11-05 20:41:45 -0600

    remove 404 urls

--- run.sh
+++ run.sh
@@ -4,20 +4,13 @@
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/USPresCG.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/USPresLG.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/USPresCty.txt
-/opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/USPresPct.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/ussenate.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/ussenatecg.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/ussenatecty.txt
-/opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/ussenatepct.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/ushouse.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/USHouseCty.txt
-/opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/ushousepct.txt
-/opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/stsenate.txt
-/opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/StateSenateByPct.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/LegislativeByDistrict.txt
-/opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/sthousepct.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/allracesbycounty.txt
-/opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/allracesbyprecinct.txt
 /opt/homebrew/bin/wget --mirror https://electionresultsfiles.sos.state.mn.us/20241105/pctstats.txt
 
 TIMESTAMP=`date`
